988e67533c98179b533de9e6bbe668caeecc6aeb
codernishchay <nishchay.vns2000@gmaiil.com>
2022-04-25 05:59:59 -0400

    added frontend

---
+++ src/fakeData/breakfast.js
@@ -0,0 +1,69 @@
+var breakfast = [
+  {
+    id: "1",
+    name: "Begal and Cream Cheese",
+    restaurant: "A Salt & Battery",
+    category: "breakfast",
+    img: "https://i.imgur.com/BaS2G0b.png",
+    description: "Pecorino smellY cheese melted cheese",
+    story:
+      "Gummi bears topping pie pastry bonbon cupcake caramels. Muffin muffin jujubes donut",
+    price: 13.99,
+  },
+  {
+    id: "2",
+    name: "Breakfast Sandwich",
+    restaurant: "A Salt & Battery",
+    category: "breakfast",
+    img: "https://i.imgur.com/zSbAh5u.png",
+    description: "Pecorino smellY cheese melted cheese",
+    story:
+      "Gummi bears topping pie pastry bonbon cupcake caramels. Muffin muffin jujubes donut",
+    price: 10.99,
+  },
+  {
+    id: "3",
+    name: "Baked Chicken fry",
+    restaurant: "A Salt & Battery",
+    category: "breakfast",
+    img: "https://i.imgur.com/lZCtGhb.png",
+    description: "Pecorino smellY cheese melted cheese",
+    story:
+      "Gummi bears topping pie pastry bonbon cupcake caramels. Muffin muffin jujubes donut",
+    price: 6.99,
+  },
+  {
+    id: "4",
+    name: "Eggs Fried Benedict",
+    restaurant: "A Salt & Battery",
+    category: "breakfast",
+    img: "https://i.imgur.com/KUJHCRC.png",
+    description: "Pecorino smellY cheese melted cheese",
+    story:
+      "Gummi bears topping pie pastry bonbon cupcake caramels. Muffin muffin jujubes donut",
+    price: 8.99,
+  },
+  {
+    id: "5",
+    name: "Toast Croissant Fried Egg",
+    restaurant: "A Salt & Battery",
+    category: "breakfast",
+    img: "https://i.imgur.com/DPRhhsO.png",
+    description: "Pecorino smellY cheese melted cheese",
+    story:
+      "Gummi bears topping pie pastry bonbon cupcake caramels. Muffin muffin jujubes donut",
+    price: 12.99,
+  },
+  {
+    id: "6",
+    name: "Full Egg Toast Brunch",
+    restaurant: "A Salt & Battery",
+    category: "breakfast",
+    img: "https://i.imgur.com/Uo1nw53.png",
+    description: "Pecorino smellY cheese melted cheese",
+    story:
+      "Gummi bears topping pie pastry bonbon cupcake caramels. Muffin muffin jujubes donut",
+    price: 9.99,
+  },
+];
+export default breakfast;
